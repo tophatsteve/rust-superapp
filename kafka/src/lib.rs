c32e6d3d67c933f7dbd49e961ef72ec32829810b
Steve Bentley <steve.bentley@greatstate.co>
2023-07-26 13:15:24 +0100

    Add kafka code

--- kafka/src/lib.rs
+++ kafka/src/lib.rs
@@ -1,14 +1,87 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod consumer;
+pub mod producer;
+pub mod util;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    use apache_avro::AvroSchema;
+    use serde::{Deserialize, Serialize};
+    use tokio::sync::mpsc;
+
+    use crate::{consumer::KafkaConsumer, producer::KafkaProducer};
+
+    #[tokio::test]
+    async fn test_produce() {
+        let topic = "string-topic";
+        let key = "test-key";
+        let payload = "test-payload";
+
+        let kafka_producer = KafkaProducer::new(
+            "localhost:9092".to_string(),
+            "http://localhost:8081".to_string(),
+            topic.to_string(),
+        );
+        let kakfa_consumer = KafkaConsumer::new(
+            "localhost:9092".to_string(),
+            "http://localhost:8081".to_string(),
+            "string-consumer".to_string(),
+            topic.to_string(),
+        );
+
+        let (sender, mut receiver) = mpsc::unbounded_channel::<String>();
+        let produce_result = kafka_producer
+            .produce(key.to_string(), payload.to_string())
+            .await;
+        assert!(produce_result);
+        let handle = tokio::spawn(async move {
+            kakfa_consumer.consume(sender.clone()).await;
+        });
+
+        while let Some(message) = receiver.recv().await {
+            assert_eq!(payload.to_string(), message);
+            break;
+        }
+        handle.abort();
+    }
+
+    #[derive(PartialEq, Eq, Debug, Serialize, Deserialize, Clone, AvroSchema)]
+    struct Custom {
+        value: String,
+    }
+
+    #[tokio::test]
+    async fn test_custom_struct_produce() {
+        let topic = "custom-struct-topic";
+        let key = "test-key";
+        let payload = Custom {
+            value: "test-payload".to_string(),
+        };
+        let kafka_producer = KafkaProducer::new(
+            "localhost:9092".to_string(),
+            "http://localhost:8081".to_string(),
+            topic.to_string(),
+        );
+        let kakfa_consumer = KafkaConsumer::new(
+            "localhost:9092".to_string(),
+            "http://localhost:8081".to_string(),
+            "custom-consumer".to_string(),
+            topic.to_string(),
+        );
+
+        let produce_result = kafka_producer
+            .produce(key.to_string(), payload.clone())
+            .await;
+        assert!(produce_result);
+        let (sender, mut receiver) = mpsc::unbounded_channel::<Custom>();
+        let handle = tokio::spawn(async move {
+            kakfa_consumer.consume(sender.clone()).await;
+        });
+
+        while let Some(message) = receiver.recv().await {
+            assert_eq!(payload, message);
+            break;
+        }
+        handle. Abort()
     }
 }
